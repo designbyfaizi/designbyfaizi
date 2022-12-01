c2ce739f8bb00a344314d55bed097de81c4fd5bc
Faizan Ullah <faizanullah1999@gmail.com>
2022-12-02 02:10:00 +0500

    Hero Section Updated

---
+++ components/Sada/Button.vue
@@ -0,0 +1,12 @@
+<template>
+    <button
+        :class="[
+            'bg-primary text-white rounded-lg border-none outline-none',
+            'font-medium px-[50px] py-[15px] cursor-pointer',
+            'hover:bg-primary-dark active:scale-96',
+            'transition-transform duration-100'
+        ]"
+    >
+        Say Hello
+    </button>
+</template>
--- components/Top/Header.vue
+++ components/Top/Header.vue
@@ -1,28 +1,35 @@
 <template>
-    <header
-        :class="`bg-backgroundsecond/70 
-      backdrop-blur-xl sticky top-[10px] container sm:mx-auto h-[70px] 
+    <header class="flex justify-center relative mx-[10px] sm:mx-auto z-100">
+        <div
+            :class="`headerContainer bg-backgroundsecond/50 
+      backdrop-blur-md fixed top-[10px] container h-[70px] 
       sm:h-[100px] flex items-center justify-between rounded-2xl p-[20px] 
-      transition-all duration-150 ring-2 ring-neutral-500/5 shadow-xl shadow-shadow/15`"
-    >
-        <TopSiteLogo />
-        <div class="flex gap-4">
-            <TopThemeIcon/>
-            <div
-                @click="dropdown = !dropdown"
-                class="aspect-square h-[30px] w-[30px] sm:h-[50px] sm:w-[50px] ring-2 ring-neutral-600 rounded-full grid place-items-center group hover:ring-primary/10 cursor-pointer active:scale-95 transition-all duration-75"
-            >
-                <Icon
-                    name="line-md:account"
-                    class="aspect-square h-[26px] w-[26px] sm:h-[46px] sm:w-[46px] text-neutral-600 group-hover:text-background group-hover:bg-primary bg-background rounded-full m-0 p-0 transition-all duration-75"
-                />
+      transition-all duration-150 ring-2 ring-neutral-500/10 shadow-xl shadow-shadow/15`"
+        >
+            <TopSiteLogo />
+            <div class="flex gap-4">
+                <TopThemeIcon />
+                <div
+                    @click="dropdown = !dropdown"
+                    class="aspect-square h-[30px] w-[30px] sm:h-[50px] sm:w-[50px] ring-2 ring-neutral-600 rounded-full grid place-items-center group hover:ring-primary/10 cursor-pointer active:scale-95 transition-all duration-75"
+                >
+                    <Icon
+                        name="line-md:account"
+                        class="aspect-square h-[26px] w-[26px] sm:h-[46px] sm:w-[46px] text-neutral-600 group-hover:text-background group-hover:bg-primary bg-background rounded-full m-0 p-0 transition-all duration-75"
+                    />
+                </div>
             </div>
+            <TopTestDropdown :dropdown="dropdown" />
         </div>
-        <TopTestDropdown :dropdown="dropdown"/>
     </header>
 </template>
 
 <script setup lang="ts">
-const { $isDarkMode } = useNuxtApp();
 const dropdown = useState("dropdown", () => false);
 </script>
+
+<style lang="scss">
+.headerContainer{
+    width: calc(100% - 20px);
+}
+</style>
