c56151a2fc5961942f27b29292a8f92f97939291
Faizan Ullah <faizanullah1999@gmail.com>
2023-10-22 03:20:19 +0500

    Testing Meta and SEO Composables

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -3,9 +3,13 @@ import { colorMode } from "./configs/colorMode.config";
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
     app: {
-        pageTransition: { name: 'page', mode: 'out-in' },
-        layoutTransition: { name: 'layout', mode: 'out-in' }
-      },
+        pageTransition: { name: "page", mode: "out-in" },
+        layoutTransition: { name: "layout", mode: "out-in" },
+        head: {
+            charset: "utf-8",
+            viewport: "width=device-width, initial-scale=1",
+        },
+    },
     ssr: true,
     css: [
         "@/assets/styles/reset.scss",
--- pages/index.vue
+++ pages/index.vue
@@ -7,11 +7,26 @@
 
 <script setup lang="ts">
 useHead({
-    title: "Design by Faizi",
-    meta: [{ name: "description", content: "My Amazing Site!" }],
-});
+  title: 'Design by Faizi',
+  meta: [
+    { name: 'description', content: 'My Amazing Site!' }
+  ],
+  bodyAttrs: {
+    class: 'test'
+  },
+  script: [ { innerHTML: 'console.log(\'Hello Homepage\')' } ]
+})
 
 definePageMeta({
     layout: "main-layout",
 });
+
+useSeoMeta({
+  title: 'Design by Faizi',
+  ogTitle: 'Design by Faizi',
+  description: `This is Faizan's amazing site`,
+  ogDescription: `This is Faizan's amazing site`,
+  ogImage: 'https://fireship.io/courses/nextjs/img/featured.webp',
+  twitterCard: 'summary_large_image',
+})
 </script>
