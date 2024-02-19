6938888ce562cfd5859e706f7645fcb3b5509be2
Faizan Ullah <faizanullah1999@gmail.com>
2024-02-20 01:57:17 +0500

    Added caution header on top

--- app.vue
+++ app.vue
@@ -1,9 +1,18 @@
 <template>
-    <NuxtLayout>
-        <NuxtLoadingIndicator />
-        <!-- Adding Key to Fix Strange Navigation Error During Fast Navigation -->
-        <NuxtPage :key="$route.fullPath" />
-    </NuxtLayout>
+  <NuxtLayout>
+    <NuxtLoadingIndicator />
+    <div
+      class="h-[40px] bg-primary text-white font-ubuntu flex gap-2 items-center justify-center p-2"
+    >
+      <h1>{{ "⚠️ This website is under construction" }}</h1>
+      <h1>
+        Contact me at
+        <a class="p-2 rounded-lg hover:bg-white/20" href="mailto:faizanullah1999@gmail.com">faizanullah1999@gmail.com</a>
+      </h1>
+    </div>
+    <!-- Adding Key to Fix Strange Navigation Error During Fast Navigation -->
+    <NuxtPage :key="$route.fullPath" />
+  </NuxtLayout>
 </template>
 
 <style>
