7dd73ae6f1d78c0b00e384cbe0d8e9f734d48d85
Faizan Ullah <faizanullah1999@gmail.com>
2025-05-05 22:03:54 +0200

    added grid bg class

--- app/layouts/default.vue
+++ app/layouts/default.vue
@@ -1,9 +1,28 @@
 <template>
-  <div class="flex flex-col gap-4 flex-1">
+  <div class="flex flex-col gap-4 flex-1 grid-bg">
     <slot />
   </div>
 </template>
 
 <script lang="ts" setup></script>
 
-<style></style>
+<style>
+.grid-bg {
+
+  --charcoal: var(--foreground);
+  background-image: repeating-linear-gradient(
+      to right,
+      var(--charcoal),
+      var(--charcoal) 1px,
+      transparent 1px,
+      transparent 30px
+    ),
+    repeating-linear-gradient(
+      to bottom,
+      var(--charcoal),
+      var(--charcoal) 1px,
+      transparent 1px,
+      transparent 30px
+    );
+}
+</style>
