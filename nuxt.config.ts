c02a304469c0830907d91342e4e6dcb10108124b
Faizan Ullah <faizanullah1999@gmail.com>
2026-05-22 07:11:45 +0200

    fix: removed ssr false

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,28 +1,27 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  compatibilityDate: '2024-11-01',
-  ssr: false,
+  compatibilityDate: "2024-11-01",
   runtimeConfig: {
     public: {
       site_url: process.env.SITE_URL!,
-      email: 'faizanullah1999@gmail.com',
+      email: "faizanullah1999@gmail.com",
       google_sheets_url: process.env.GOOGLE_SHEETS_URL!,
     },
     api_url: process.env.API_URL!,
     api_key: process.env.API_KEY!,
   },
   future: {
-    compatibilityVersion: 4
+    compatibilityVersion: 4,
   },
   modules: [
-    '@unocss/nuxt',
-    '@vueuse/nuxt',
-    '@nuxt/icon',
-    '@nuxt/image',
-    'reka-ui/nuxt',
-    'motion-v/nuxt',
-    '@nuxt/content',
-    '@nuxt/fonts'
+    "@unocss/nuxt",
+    "@vueuse/nuxt",
+    "@nuxt/icon",
+    "@nuxt/image",
+    "reka-ui/nuxt",
+    "motion-v/nuxt",
+    "@nuxt/content",
+    "@nuxt/fonts",
   ],
   fonts: {
     families: [
@@ -39,40 +38,36 @@ export default defineNuxtConfig({
         src: "/fonts/cosmic/cosmic-sans-vf-italic.ttf",
         weights: [100, 200, 300, 400, 500, 600, 700, 800],
         styles: ["italic"],
-      }
-    ]
+      },
+    ],
   },
-  css: [
-    "@/assets/styles/main.css"
-  ],
+  css: ["@/assets/styles/main.css"],
   routeRules: {
     "/skills": { isr: true },
     "/skills/**": { isr: true },
     "/blog": { isr: true },
     "/blog/**": { isr: true },
   },
-  devtools: { enabled: process.env.ENV === 'DEV' },
+  devtools: { enabled: process.env.ENV === "DEV" },
   nitro: {
-    compatibilityDate: '2024-11-01',
+    compatibilityDate: "2024-11-01",
   },
   image: {
-    domains: [
-      process.env.SITE_URL || 'http://localhost:3000',
-    ]
+    domains: [process.env.SITE_URL || "http://localhost:3000"],
   },
   components: [
     {
       path: "@/components",
-      pathPrefix: true
+      pathPrefix: true,
     },
     {
-      path: '@/components/ui',
-      pathPrefix: false
-    }
+      path: "@/components/ui",
+      pathPrefix: false,
+    },
   ],
   vite: {
     server: {
-      allowedHosts: true
-    }
-  }
-})
+      allowedHosts: true,
+    },
+  },
+});
